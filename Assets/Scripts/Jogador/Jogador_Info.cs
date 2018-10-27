c7fece664778f08b39bf7c2af39138d02b55891d
guilhermejucaol <guilhermejucaol@gmail.com>
2018-10-27 00:12:00 -0300

    Player Movement

--- Assets/Scripts/Jogador/Jogador_Info.cs
+++ Assets/Scripts/Jogador/Jogador_Info.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Jogador_Info : MonoBehaviour {
 
 	
-	private Tile FacingTile;
+	public Vector3Int FacingTile;
 
 	void Start () {
 		
--- Assets/Scripts/Jogador/Jogador_Movimento.cs
+++ Assets/Scripts/Jogador/Jogador_Movimento.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 
 public class Jogador_Movimento : MonoBehaviour {
 
+/* Tilemaps */
 	public Tilemap Chao;
+	public Tilemap Obstaculos;
+/* Tilemaps */
+
+/* Referencias Externas */
+	private Jogador_Info PlayerInfo;
+/* Referencias Externas */
+
+/* Variaveis */
 	public float MoveSpeed;
 	public bool Moving;
+/* Variaveis */
+
+/* Lerp de movimento do personagem (triggers de animacao coloca aqui tbm) */
 
 	private IEnumerator Mover(Vector3 nextPos){
-		print(nextPos.ToString());
 		Moving = true;
 		while(Moving){
 			if(Vector3.Distance(transform.position, nextPos) > 0){
@@ -22,25 +33,66 @@ public class Jogador_Movimento : MonoBehaviour {
 		}
 		yield return null;
 	}
+/* Lerp de movimento do personagem (triggers de animacao coloca aqui tbm) */
 
+/* Atualizar Tile de interação */
+	private void UpdateFacingTile(Vector3Int toUpdate){
+			Chao.SetTileFlags(PlayerInfo.FacingTile, TileFlags.None);
+			Chao.SetColor(PlayerInfo.FacingTile, Color.white);
+			PlayerInfo.FacingTile = toUpdate;
+			Chao.SetTileFlags(PlayerInfo.FacingTile, TileFlags.None);
+			Chao.SetColor(PlayerInfo.FacingTile, Color.red);		
+	}
+/* Atualizar Tile de interação */
 
 	void Start () {
-		
+		PlayerInfo = transform.GetComponent<Jogador_Info>();
 	}
 	
-	
 	void Update () {
-		if((Input.GetKeyDown("up") || Input.GetKeyDown("down") || Input.GetKeyDown("left") || Input.GetKeyDown("right")) && !Moving){
-			float h = Input.GetAxis("Horizontal");
-			float v = Input.GetAxis("Vertical");
-			print(h);
-			print(v);
+		if(!Moving){
 			Vector3 pos = transform.position;
-			Vector3Int destination = Chao.WorldToCell(new Vector3(pos.x+Chao.cellSize.x * h,pos.y+Chao.cellSize.y * v,0));
-			print(pos.ToString());
-			print(destination.ToString());
-			
-			Mover(Chao.GetCellCenterWorld(destination));
+			if(Input.GetKey("up")){
+
+				Vector3Int destination = Chao.WorldToCell(new Vector3(pos.x,pos.y+Chao.cellSize.y,0));
+				UpdateFacingTile(destination);
+				
+				if(Chao.HasTile(destination) && !Obstaculos.HasTile(destination)){
+					UpdateFacingTile(Chao.WorldToCell(new Vector3(pos.x,pos.y+Chao.cellSize.y*2,0)));
+					StopAllCoroutines();
+					StartCoroutine(Mover(Chao.GetCellCenterWorld(destination)));
+				}
+			}else if(Input.GetKey("down")){
+
+				Vector3Int destination = Chao.WorldToCell(new Vector3(pos.x,pos.y-Chao.cellSize.y,0));
+				UpdateFacingTile(destination);
+				
+				if(Chao.HasTile(destination) && !Obstaculos.HasTile(destination)){
+					UpdateFacingTile(Chao.WorldToCell(new Vector3(pos.x,pos.y-Chao.cellSize.y*2,0)));
+					StopAllCoroutines();
+					StartCoroutine(Mover(Chao.GetCellCenterWorld(destination)));	
+				}
+			}else if(Input.GetKey("left")){
+
+				Vector3Int destination = Chao.WorldToCell(new Vector3(pos.x-Chao.cellSize.x,pos.y,0));
+				UpdateFacingTile(destination);
+				
+				if(Chao.HasTile(destination) && !Obstaculos.HasTile(destination)){
+					UpdateFacingTile(Chao.WorldToCell(new Vector3(pos.x-Chao.cellSize.x*2,pos.y,0)));
+					StopAllCoroutines();
+					StartCoroutine(Mover(Chao.GetCellCenterWorld(destination)));
+				}
+			}else if(Input.GetKey("right")){
+
+				Vector3Int destination = Chao.WorldToCell(new Vector3(pos.x+Chao.cellSize.x,pos.y,0));
+				UpdateFacingTile(destination);
+				
+				if(Chao.HasTile(destination) && !Obstaculos.HasTile(destination)){
+					UpdateFacingTile(Chao.WorldToCell(new Vector3(pos.x+Chao.cellSize.x*2,pos.y,0)));
+					StopAllCoroutines();
+					StartCoroutine(Mover(Chao.GetCellCenterWorld(destination)));
+				}
+			}
 		}
 	}
 }
